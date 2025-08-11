79f6fbb02d6f6cc149ee2f4a2ab7dc4c2b1c5f8d
Richard Obaze <richardobaze@yahoo.co.uk>
2025-08-11 03:13:07 +0100

    Added CI for github and Axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000)); // write address like this to not make typos
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
