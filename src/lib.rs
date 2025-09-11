2db58455684c4d19f5374e3aadcd31d08f1087e7
Richard Obaze <richardobaze@yahoo.co.uk>
2025-09-11 03:53:41 +0100

    Updated tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    routing::{get, post},
+};
 use simplelog::*;
 
 // Handler that immediately returns an empty `200 OK` response.
 async fn health_check() {}
 
+// Handler that immediately returns an empty `200 OK` response.
+async fn subscribe() {}
+
 pub async fn app() -> Router {
     // build our application routes
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/health_check", get(health_check));
+        .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscribe));
     info!("Created router app");
 
     app
